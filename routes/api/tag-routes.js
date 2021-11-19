ff69a66b6b4a16edb98184865125ea2eea90f038
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-19 15:53:32 -0600

    find one tag GET route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,6 +24,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock"]
+      }
+      
+    ]
+  }).then(dbResposne => res.json(dbResposne))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
