93144b6a49505c32d7a25b99803778dc8c52b40c
Noah Tidwell <ntidwell34@yahoo.com>
2021-11-19 15:57:54 -0600

    update find one tag GET route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,8 +30,8 @@ router.get('/:id', (req, res) => {
     },
     include: [
       {
-        model: Product,
-        attributes: ["id", "product_name", "price", "stock"]
+        model: ProductTag,
+        attributes: ["id", "product_id", "tag_id"]
       }
       
     ]
